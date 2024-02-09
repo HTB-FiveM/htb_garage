9d9f33aadb1d8e693c5bb17ad606ca2e9bfd6618
Cx-Developments <71113137+Cx-Developments@users.noreply.github.com>
2024-02-09 15:28:35 +1100

    Update VehicleStuff.lua
    Modify and remove GetHashKey and use jenkins hashing as it's a faster method to invoke a result for a model hashkey

--- VehicleStuff.lua
+++ VehicleStuff.lua
@@ -1,50 +1,50 @@
-
 HtbGarageUtil = {}
+
 function HtbGarageUtil.RequestModel(modelHash, cb)
-	modelHash = (type(modelHash) == 'number' and modelHash or GetHashKey(modelHash))
-
-	if not HasModelLoaded(modelHash) and IsModelInCdimage(modelHash) then
-		HtbGarageUtil.RequestModel(modelHash)
-
-		while not HasModelLoaded(modelHash) do
-			Citizen.Wait(4)
-		end
-	end
-
-	if cb ~= nil then
-		cb()
-	end
+    modelHash = (type(modelHash) == 'number' and modelHash or `modelHash`) -- Using backticks instead of GetHashKey
+    
+    if not HasModelLoaded(modelHash) and IsModelInCdimage(modelHash) then
+        HtbGarageUtil.RequestModel(modelHash)
+    
+        while not HasModelLoaded(modelHash) do
+            Citizen.Wait(4)
+        end
+    end
+    
+    if cb ~= nil then
+        cb()
+    end
 end
 
 function SpawnVehicle(vehicle, coords, heading, cb, networked)
-	local model = (type(vehicle) == 'number' and vehicle or GetHashKey(vehicle))
-	local vector = type(coords) == "vector3" and coords or vec(coords.x, coords.y, coords.z)
-	networked = networked ~= nil and networked ~= false -- Ensure networked will never be nil
-	Citizen.CreateThread(function()
-		HtbRequestModel(model)
-
-		local vehicle = CreateVehicle(model, vector.x, vector.y, vector.z, heading, networked, false)
-
-		local networkId = nil
-		if networked == true then
-			networkId = NetworkGetNetworkIdFromEntity(vehicle)
-			SetNetworkIdCanMigrate(networkId, true) -- Allow player interaction
-			SetEntityAsMissionEntity(vehicle, true, false) -- Ensure no automatic despawn
-		end
-		SetVehicleHasBeenOwnedByPlayer(vehicle, true)
-		SetVehicleNeedsToBeHotwired(vehicle, false)
-		SetModelAsNoLongerNeeded(model)
-		SetVehRadioStation(vehicle, 'OFF')
-
-		RequestCollisionAtCoord(vector.xyz)
-		while not HasCollisionLoadedAroundEntity(vehicle) do
-			Citizen.Wait(0)
-		end
-
-		if cb then
-			cb(vehicle, networkId)
-		end
-	end)
+    local model = (type(vehicle) == 'number' and vehicle or `vehicle`) -- Using backticks instead of GetHashKey
+    local vector = type(coords) == "vector3" and coords or vec(coords.x, coords.y, coords.z)
+    networked = networked ~= nil and networked ~= false -- Ensure networked will never be nil
+    Citizen.CreateThread(function()
+        HtbRequestModel(model)
+    
+        local vehicle = CreateVehicle(model, vector.x, vector.y, vector.z, heading, networked, false)
+    
+        local networkId = nil
+        if networked == true then
+            networkId = NetworkGetNetworkIdFromEntity(vehicle)
+            SetNetworkIdCanMigrate(networkId, true) -- Allow player interaction
+            SetEntityAsMissionEntity(vehicle, true, false) -- Ensure no automatic despawn
+        end
+        SetVehicleHasBeenOwnedByPlayer(vehicle, true)
+        SetVehicleNeedsToBeHotwired(vehicle, false)
+        SetModelAsNoLongerNeeded(model)
+        SetVehRadioStation(vehicle, 'OFF')
+    
+        RequestCollisionAtCoord(vector.xyz)
+        while not HasCollisionLoadedAroundEntity(vehicle) do
+            Citizen.Wait(0)
+        end
+    
+        if cb then
+            cb(vehicle, networkId)
+        end
+    end)
 end
 
 
