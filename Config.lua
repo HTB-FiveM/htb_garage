1e54e354df2cc79eb5cfac9a76d490ef104c2346
steveski <steve@steve-york.com>
2025-04-24 14:18:47 +1000

    feat: reverted the impound time periods

--- Config.lua
+++ Config.lua
@@ -34,8 +34,8 @@ Config.AllowedImpoundJobs = {
     mechanic = true
 }
 
---Config.ImpoundTimePeriods = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}
-Config.ImpoundTimePeriods = {0,1,2,3}
+Config.ImpoundTimePeriods = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}
+--Config.ImpoundTimePeriods = {0,1,2,3}
 
 -- Set the identifier type as a string here, for example:
 --		steam
