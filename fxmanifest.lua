e601ca0a79252f37e05b3d4ddab89bef0ae6f67d
Playooze <92321525+Playooze@users.noreply.github.com>
2025-05-03 15:27:08 +0100

    Changes on fxmanifest
    Fixed some files not being tagged on fxmanifest
    Removed the "dependency" of esx/qbcore framework written
    Added some commented text giving instructions on which lines to comment when using ESX/QB-Core

---
+++ fxmanifest.lua
@@ -0,0 +1,44 @@
+fx_version("cerulean")
+game("gta5")
+
+author("Harry The Bastard")
+version("1.3.0")
+description("A better garage system")
+lua54 'yes'
+
+ui_page("ui/index.html")
+
+files({
+	"UI/index.html",
+})
+
+shared_scripts({
+	--"@qb-core/shared/locale.lua",  -- uncomment if using QB-Core
+	"@es_extended/imports.lua",  -- comment if using QB-Core
+	"@es_extended/locale.lua",  -- comment if using QB-Core
+
+	"locales/locale.lua",
+	"locales/en.lua",
+	"common.lua",
+})
+
+server_scripts({
+	"@oxmysql/lib/MySQL.lua",
+	"Config.lua",
+	"Server/utils.lua",
+	"FrameworkSpecific/Interfaces.lua",
+	"FrameworkSpecific/Server/*.lua",
+	"FrameworkSpecific/Setup_Server.lua",
+	"FrameworkSpecific/SQL/*.lua",
+	"Server/main.lua",
+})
+
+client_scripts({
+	"Config.lua",
+	"Configs/*.lua",
+	"FrameworkSpecific/Interfaces.lua";
+	"VehicleStuff.lua",
+	"FrameworkSpecific/Client/*.lua",
+	"FrameworkSpecific/Setup_Client.lua",
+	"Client/*.lua",
+})
